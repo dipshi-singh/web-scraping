a8e616d0d348087813b6af8177c11ccd5adb2a61
Dipshi <uselessmails101@gmail.com>
2023-12-20 22:13:34 +0530

    scrolling and saving html sourcecode

--- Scrapper.py
+++ Scrapper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
 
 
 import time
@@ -8,14 +9,39 @@ import time
 #open browser
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=chrome_options)
 
-# Navigate to google
-driver.get('https://www.google.com')
+prefs = {"profile.default_content_setting_values.geolocation": 2}
+chrome_options.add_experimental_option("prefs", prefs)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://www.ajio.com/men-backpacks/c/830201001')
+
+
+# Navigate to ajio
 time.sleep(2)
 
-#search on google
-input = driver.find_element(by=By.XPATH, value='//*[@id="APjFqb"]')
-input.send_keys('selenium')
-time.sleep(1)
-input.send_keys(Keys.ENTER)
+#find the height of the current page
+height = driver.execute_script('return document.body.scrollHeight')
+print(height)
+
+#scroll to last
+old_height = driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+
+#loop till the last row of items
+while True:
+    driver.execute_script('return document.body.scrollHeight')
+    time.sleep(2)
+
+    new_height = driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+
+    if new_height == old_height:
+        break
+
+    old_height=new_height
+
+html = driver.page_source
+
+with open('ajio.html','w',encoding='utf-8') as f:
+    f.write(html)
+
+    
